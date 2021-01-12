cdef37d2fe90d8c32ba1ae33467231ea65b3aea7
glinomes227 <77336360+glinomes227@users.noreply.github.com>
2021-01-12 16:42:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// let burger = document.getElementsByClassName('menu');
+// let remove = document.getElementsByClassName('btn-remove');
+// let nav = document.getElementsByClassName('nav-list');
+
+// burger.addEventListener('click', function(){
+//     nav.classList.toggle('nav-list-activ');
+// })
+const windowWidth = window.innerWidth;
+$('.menu').click(function(){
+    $('.nav-list').toggleClass('nav-list-activ');
+    $('.content').toggleClass('content-activ');
+    if (windowWidth <= 480) {
+        $('.content').removeClass('content-activ');
+    }
+
+})
+$('.btn-remove').click(function(){
+    $('.nav-list').removeClass('nav-list-activ');
+})
